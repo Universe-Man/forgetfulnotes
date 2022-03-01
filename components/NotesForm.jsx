203290d514440ba05457fc4503c0d05b878ef8f1
Ian Pollack <ianjpollack@gmail.com>
2022-02-28 19:39:11 -0800

    set up 1st forgetful note

--- components/NotesForm.jsx
+++ components/NotesForm.jsx
@@ -9,30 +9,50 @@ const NotesForm = (props) => {
   // 5 notes = honestly i can't remember this at all
 
   useEffect(() => {
-    let notes = document.getElementsByClassName('noteCard');
-    console.log(props.notesList)
+    // let notes = document.getElementsByClassName('noteCard');
+    console.log(props.notesList) // [...9, 10, 11, 12, 13] length - 3/4/5
     // debugger;
     // 3 notes
-    if (props.notesList.length > 2) {
+    if (props.notesList.length === 3) { // [0, 1, 2] Length = 3
+      props.notesList[props.notesList.length - 3] // [0]
+      let note = document.getElementById(`noteCard-${props.notesList.length - 3}`);
+      console.log(note);
+      // debugger;
+      note.style.color = 'rgba(0, 0, 0, 0)';
+      setTimeout(() => {
+        // debugger;
+        note.innerHTML = 'umm...err...what?';
+        note.style.color = 'rgba(0, 0, 0, 1)';
+      }, 2000);
+      // setTimeout(() => {
+      // }, 3000);
+
+      // for (let i = 0; i < props.notesList.length; i++) {
+      //   props.notesList
+      // }
+      // 3 NOTES
       // first note in list
       // fade out
-      // console.log(props.notesList[0], '3 or more');
-      // notes[0].classList.add('${styles.fade-away}');
-      // props.notesList[0].style.color = 'rgba(0, 0, 0, 0.1)';
-      let firstNote = document.getElementById('noteCard-0');
+      // let firstNote = document.getElementById('noteCard-0');
       // debugger;
       // console.log(firstNote, 'hi look');
-      firstNote.style.color = 'rgba(0, 0, 0, 0.1)';
-      firstNote.innerHTML = // new text
+      // firstNote.style.color = 'rgba(0, 0, 0, 0.1)';
+      // firstNote.innerHTML = // new text
 
 
 
-        props.notesList[0] = 'yay'
+      // props.notesList[0] = 'yay'
       // notes[0].classList.remove('fade-away');
 
       // change text
       // fade in
+    } else if (props.notesList.length === 4) {
+      // 4 NOTES
+
+    } else if (props.notesList.length >= 5) {
+      // 5+ NOTES
     }
+
   }, [props.notesList])
 
   const handleChange = (event) => {
